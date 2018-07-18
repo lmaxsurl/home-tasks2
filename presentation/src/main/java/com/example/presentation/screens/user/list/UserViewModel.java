54b2d213059cd9140065654f7ea1c76fb37f266b
lmaxsurl <maxim.logunov.1998@mail.ru>
2018-07-18 09:02:09 +0300

    home task 9

--- presentation/src/main/java/com/example/presentation/screens/user/list/UserViewModel.java
+++ presentation/src/main/java/com/example/presentation/screens/user/list/UserViewModel.java
@@ -12,14 +12,14 @@ import jp.wasabeef.picasso.transformations.CropCircleTransformation;
 
 public class UserViewModel extends BaseViewModel {
 
-    private final int MALE_COLOR = Color.rgb(70, 130, 180);
-    private final int FEMALE_COLOR = Color.rgb(255, 20, 147);
+    private static final int MALE_COLOR = Color.rgb(70, 130, 180);
+    private static final int FEMALE_COLOR = Color.rgb(255, 20, 147);
+    private static final String KEY_MALE = "male";
     public ObservableField<String> firstName = new ObservableField<>("Mark");
     public ObservableField<String> surname = new ObservableField<>("Fender");
     public ObservableField<String> gender = new ObservableField<>("Male");
     public ObservableField<String> age = new ObservableField<>("23");
     public ObservableField<String> imageUrl = new ObservableField<>("http://images.hellokids.com/_uploads/_tiny_galerie/20130520/dhh_how-to-draw-mike-wazowski-easy-tutorial-drawing.png");
-    public ObservableField<Integer> backgroundColor = new ObservableField<>(MALE_COLOR);
 
     @BindingAdapter("imageUrl")
     public static void loadImage(ImageView view, String url) {
@@ -30,8 +30,8 @@ public class UserViewModel extends BaseViewModel {
     }
 
     @BindingAdapter("android:background")
-    public static void setBackground(View view, int color) {
-        view.setBackgroundColor(color);
+    public static void setBackground(View view, String gender) {
+        view.setBackgroundColor(gender.equalsIgnoreCase(KEY_MALE)?MALE_COLOR:FEMALE_COLOR);
     }
 }
 
